c0242d54b6900ce46a6231068a9d905fd9744957
qinrongchang <846581944@qq.com>
2017-08-07 17:24:47 +0800

    物流端 注册

--- transportation/src/main/java/com/arkui/transportation/activity/user/RegisterActivity.java
+++ transportation/src/main/java/com/arkui/transportation/activity/user/RegisterActivity.java
@@ -1,15 +1,55 @@
 package com.arkui.transportation.activity.user;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.arkui.fz_tools.ui.BaseActivity;
+import com.arkui.fz_tools.model.Constants;
+import com.arkui.fz_tools.mvp.BaseMvpActivity;
+import com.arkui.fz_tools.mvp.UserInterface;
+import com.arkui.fz_tools.mvp.UserModel;
+import com.arkui.fz_tools.mvp.UserPresenter;
+import com.arkui.fz_tools.utils.TimeCountUtil;
+import com.arkui.fz_tools.utils.ToastUtil;
+import com.arkui.fz_tools.view.ShapeButton;
+import com.arkui.fz_tools.view.ShapeEditText;
+import com.arkui.fz_tools.view.ShapeLinearLayout;
 import com.arkui.transportation.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static com.arkui.transportation.R.id.et_new_password;
+import static com.arkui.transportation.R.id.et_phone;
 
-public class RegisterActivity extends BaseActivity {
+
+public class RegisterActivity extends BaseMvpActivity<UserPresenter,UserModel>implements UserInterface {
+
+    @BindView(et_phone)
+    ShapeEditText etPhone;
+    @BindView(R.id.et_code)
+    EditText etCode;
+    @BindView(R.id.tv_code)
+    TextView tvCode;
+    @BindView(R.id.ll_code)
+    ShapeLinearLayout llCode;
+    @BindView(et_new_password)
+    ShapeEditText etNewPassword;
+    @BindView(R.id.et_confirm_password)
+    ShapeEditText etConfirmPassword;
+    @BindView(R.id.et_invite)
+    ShapeEditText etInvite;
+    @BindView(R.id.bt_register)
+    ShapeButton btRegister;
+    @BindView(R.id.tv_back_login)
+    TextView tvBackLogin;
+    @BindView(R.id.checkbox)
+    CheckBox checkbox;
+    @BindView(R.id.tv_clause)
+    TextView tvClause;
+     private TimeCountUtil mTimeCountUtil;
 
     @Override
     public void setRootView() {
@@ -23,14 +63,58 @@ public class RegisterActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.bt_register, R.id.tv_clause})
+    @Override
+    public void initData() {
+        mTimeCountUtil= new TimeCountUtil(tvCode);
+    }
+
+    @OnClick({R.id.bt_register, R.id.tv_clause,R.id.tv_code})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bt_register:
+                if (checkbox.isChecked()){
+                    getRegister();
+                }else {
+                    ToastUtil.showToast(RegisterActivity.this,"免责声明未勾选");
+                }
+                break;
+            case R.id.tv_code:
+                String phoneNumber = etPhone.getText().toString().trim();
+                mPresenter.getCode(phoneNumber,mTimeCountUtil);
                 break;
             case R.id.tv_clause:
                 showActivity(ClauseActivity.class);
                 break;
+            case R.id.tv_back_login:
+                finish();
+                break;
         }
+    }
+
+    /**
+     * 注册
+     */
+    private void getRegister() {
+        String phoneNumber = etPhone.getText().toString().trim();
+        String code = etCode.getText().toString().trim();
+        String newPassWord = etNewPassword.getText().toString().trim();
+        String confirmPassword = etConfirmPassword.getText().toString().trim();
+        String invite = etInvite.getText().toString().trim();
+        mPresenter.getRegister(phoneNumber,code,newPassWord,confirmPassword, Constants.LOGISTICS,invite);
+
+    }
+
+    // 初始化
+    @Override
+    public void initPresenter() {
+        mPresenter.setUserInterface(this,mModel);
+    }
+
+    /**
+     *  成功的回掉
+     */
+    @Override
+    public void onSucceed() {
+           finish();
     }
 }
