a5306b710d0f8897672a9a4186474d9cbcb0fc6f
null <nmlizhibin@163.com>
2017-08-07 16:56:54 +0800

    验证码 获取更改

--- fzTools/src/main/java/com/arkui/fz_tools/mvp/UserModel.java
+++ fzTools/src/main/java/com/arkui/fz_tools/mvp/UserModel.java
@@ -1,20 +1,28 @@
 package com.arkui.fz_tools.mvp;
 
+import android.app.Activity;
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import com.arkui.fz_net.entity.BaseHttpResult;
 import com.arkui.fz_net.http.HttpMethod;
 import com.arkui.fz_net.http.RetrofitFactory;
 import com.arkui.fz_net.subscribers.ProgressSubscriber;
+import com.arkui.fz_tools.model.Constants;
 import com.arkui.fz_tools.model.UserApi;
+import com.arkui.fz_tools.model.VerifyDao;
+import com.arkui.fz_tools.net.JsonData;
+import com.arkui.fz_tools.net.ResultCallBack;
 import com.arkui.fz_tools.utils.UserType;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import io.reactivex.Observable;
+import io.reactivex.functions.Action;
 
 /**
  * Created by nmliz on 2017/8/7.
@@ -39,6 +47,22 @@ public class UserModel implements BaseModel {
         }
         Observable<BaseHttpResult> observable = RetrofitFactory.createRetrofit(UserApi.class).getRegister(parameter);
         HttpMethod.getInstance().getNetData(observable,progressSubscriber);
+    }
+
+    //获取验证码
+    void getCode(@NonNull String mobile, int verificationCode, final @NonNull Activity activity) {
+        VerifyDao.getInstance().sendVer(activity, mobile, verificationCode, new ResultCallBack() {
+            @Override
+            public void onSuccess(JsonData data) {
+
+            }
+
+            @Override
+            public void onSuccess(String string) {
+                super.onSuccess(string);
+                Toast.makeText(activity, "发送成功", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
